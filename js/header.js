684ed8ed581353c5538f3a1c6233937c8478c605
Andrew <chiron1600@gmail.com>
2025-02-18 17:21:43 +0200

    working on a burger menu

--- js/header.js
+++ js/header.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
+    const burgerMenu = document.querySelector('.burger-menu');
     const menu = document.querySelector('.headrer-navigation-style');
+    burgerMenu.classList.toggle('active');
     menu.classList.toggle('active');
 }
