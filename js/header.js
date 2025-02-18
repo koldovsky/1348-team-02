3a78f6cce6fd36316e7696689b2590ea06a52fb3
Andrew <chiron1600@gmail.com>
2025-02-18 18:51:53 +0200

    update burger menu

--- js/header.js
+++ js/header.js
@@ -1,6 +1,10 @@
 function toggleMenu() {
     const burgerMenu = document.querySelector('.burger-menu');
     const menu = document.querySelector('.header-navigation-style');
+    const time = document.querySelector('.time-contact');
+   /* const contact = document.querySelector('.contact-email-phone');*/
     burgerMenu.classList.toggle('active');
     menu.classList.toggle('active');
+    time.classList.toggle('active');
+    /*contact.classList.toggle('active');*/
 }
