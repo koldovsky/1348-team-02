a139eaeb09003e8e33e883608a4788cd029fe970
Andrew <chiron1600@gmail.com>
2025-02-18 18:11:54 +0200

    update header name

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     const burgerMenu = document.querySelector('.burger-menu');
-    const menu = document.querySelector('.headrer-navigation-style');
+    const menu = document.querySelector('.header-navigation-style');
     burgerMenu.classList.toggle('active');
     menu.classList.toggle('active');
 }
