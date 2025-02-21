014c6881e221544129a7f0469f6bcf4d3a3d5f18
wwesasha228 <wwesasha228@gmail.com>
2025-02-21 17:22:58 +0200

    Update taks02.js

--- js-tasks/taks02.js
+++ js-tasks/taks02.js
@@ -71,19 +71,11 @@ function isDivisible(n, x, y) {
     }
 }
 
-//https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
-function solution(number) {
-  if (number < 0) return 0;
-
-  let sum = 0;
-  for (let i = 0; i < number; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
-  }
-  
-  return sum;
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string) {
+  return string.split(" ");
 }
+
 
 //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
