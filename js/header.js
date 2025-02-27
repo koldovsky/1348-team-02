2b01f7e48c2d0feb3f638dd134ba5df0bbce3707
Andrew <chiron1600@gmail.com>
2025-02-27 16:22:27 +0200

    save update

--- js/header.js
+++ js/header.js
@@ -1,10 +1,22 @@
 function toggleMenu() {
     const burgerMenu = document.querySelector('.burger-menu');
-    const menu = document.querySelector('.header-navigation-style');
-    const time = document.querySelector('.time-contact');
-   /* const contact = document.querySelector('.contact-email-phone');*/
+    const navigation = document.querySelector('.header-navigation-style');
+    const timeContact = document.querySelector('.time-contact');
+    const mediaIcon = document.querySelector('.media-icon');
+    const workTime = document.querySelector('.work-time');
+    const contactEmail = document.querySelector('.contact-email');
+    const contactPhone = document.querySelector('.contact-phone');
+    const socialNetworks = document.querySelector('.social-networks');
+
     burgerMenu.classList.toggle('active');
-    menu.classList.toggle('active');
-    time.classList.toggle('active');
-    /*contact.classList.toggle('active');*/
+    navigation.classList.toggle('active');
+    timeContact.classList.toggle('active');
+    mediaIcon.classList.toggle('active');
+    workTime.classList.toggle('active');
+    contactEmail.classList.toggle('active');
+    contactPhone.classList.toggle('active');
+    socialNetworks.classList.toggle('active');
+
+    console.log('Menu toggled');
 }
+document.querySelector('.burger-menu').addEventListener('click', toggleMenu);
