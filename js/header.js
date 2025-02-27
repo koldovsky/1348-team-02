ed51150a463140fbf46818bbb4042b9e64d784e0
Andrew <99794379+AndrewKozak2@users.noreply.github.com>
2025-02-27 17:46:44 +0000

    add id

--- js/header.js
+++ js/header.js
@@ -12,4 +12,31 @@ function toggleMenu() {
   console.log("Menu toggled");
 }
 
+function closeMenu() {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const navigation = document.querySelector(".header-navigation-style");
+  const timeContact = document.querySelector(".time-contact");
+  const mediaIcon = document.querySelector(".media-icon");
+
+  burgerMenu.classList.remove("active");
+  navigation.classList.remove("active");
+  timeContact.classList.remove("active");
+  mediaIcon.classList.remove("active");
+
+  console.log("Menu closed");
+}
 document.querySelector(".burger-menu").addEventListener("click", toggleMenu);
+
+document.querySelectorAll(".header-navigation-link").forEach(link => {
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+
+    closeMenu();
+  });
+});
