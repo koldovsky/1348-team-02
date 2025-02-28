9575e8cd9da6125b76a0a1896882a129b6d5b953
Oleksandr Vasylovych <vasilovich.oleksandr@gmail.com>
2025-02-28 16:31:48 +0200

    Add JS New arrivals

---
+++ js/index.carousel-new-arrivals.js
@@ -0,0 +1,11 @@
+ const slides = document.querySelectorAll(".carousel-new-arrivals__item");
+ const prevButton = carousel.querySelector(".carousel-new-arrivals__arrow--left");
+ const nextButton = carousel.querySelector(".carousel-new-arrivals__arrow--right");
+ const carouselConteiner=document.querySelector(".carousel-new-arrivals__items")   
+   
+ let currentSlideIdx=0;
+
+   fuction renderCarousel(slides){
+    carouselContainer.innerHTML = "";
+    
+   }
