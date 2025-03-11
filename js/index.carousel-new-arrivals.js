03fb075c516e5f97428c8280dabe71fe8bf04685
Oleksandr Vasylovych <vasilovich.oleksandr@gmail.com>
2025-03-11 10:22:29 +0200

    new-arrivals

--- js/index.carousel-new-arrivals.js
+++ js/index.carousel-new-arrivals.js
@@ -1,44 +1,45 @@
- const slides = document.querySelectorAll(".carousel-new-arrivals__item");
- const prevButton = document.querySelector(".carousel-new-arrivals__arrow--left");
-const nextButton = document.querySelector(".carousel-new-arrivals__arrow--right")  
- const carouselCont=document.querySelector(".carousel-new-arrivals__items")  
- 
- let currentSlideIdx=0;
- let slidesToShow = getVisibleSlides();
- const totalSlides = slides.length;
+const slides = document.querySelectorAll(".carousel-new-arrivals__item");
+const prevButton = document.querySelector(".carousel-new-arrivals__arrow--left");
+const nextButton = document.querySelector(".carousel-new-arrivals__arrow--right");
+const carouselContainer = document.querySelector(".carousel-new-arrivals__items");
 
- function getVisibleSlides() {
-  if (window.innerWidth >= 992) return 4;
-  if (window.innerWidth >= 768) return 2;
-  return 1;
-}
-   
- function renderCarousel() {
-  
-   carouselCont.innerHTML = "";
-slidesToShow = getVisibleSlides();
-let newContent = "";
-for (let i = 0; i < slidesToShow; i++) {
-    let slideIndex = (currentSlideIdx + i) % totalSlides;
-    newContent += slides[slideIndex].outerHTML;
-}
-carouselCont.innerHTML = newContent;
- }
+let currentSlideIdx = 0;
+let visibleSlides = getVisibleSlides();
 
-   function nextSlide() {
+function getVisibleSlides() {
+    if (window.matchMedia("(min-width: 992px)").matches) return 4;
+    if (window.matchMedia("(min-width: 768px)").matches) return 2;
+    return 1;
+}
+
+function renderCarousel() {
+    carouselContainer.innerHTML = "";
+
+    visibleSlides = getVisibleSlides();
+
+    for (let i = 0; i < visibleSlides; i++) {
+        let slideIdx = (currentSlideIdx + i) % slides.length;
+        if (slideIdx < 0) {
+            slideIdx = slides.length + slideIdx;
+        }
+        carouselContainer.appendChild(slides[slideIdx].cloneNode(true));
+    }
+}
+
+function nextSlide() {
     currentSlideIdx = (currentSlideIdx + 1) % slides.length;
     renderCarousel();
-   }
+}
 
-   function prevSlide() {
-    currentSlideIdx= (currentSlideIdx - 1 + slides.length) % slides.length;
+function prevSlide() {
+    currentSlideIdx = (currentSlideIdx - 1 + slides.length) % slides.length;
     renderCarousel();
-   }
+}
 
-   
-  renderCarousel();
+renderCarousel();
 
-nextButton.addEventListener("click",nextSlide);   
+nextButton.addEventListener("click", nextSlide);
 prevButton.addEventListener("click", prevSlide);
 window.addEventListener("resize", renderCarousel);
+
 
